a86508c7f2b130253ddff008064751919fa786e8
sharif57 <sharifmahamud577951@gmail.com>
2024-11-05 09:34:54 +0600

    tnstagram section done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "./Banner";
 import BestSellers from "./BestSellers";
 import Categories from "./Categories";
 import Favorites from "./Favorites";
+import Instagram from "./Instagram";
 import SubscriptionForm from "./SubscriptionForm";
 import Summer from "./Summer";
 import Testimonials from "./Testimonials";
@@ -15,6 +16,7 @@ const Home = () => {
     <BestSellers></BestSellers>
     <Testimonials></Testimonials>
     <SubscriptionForm></SubscriptionForm>
+    <Instagram></Instagram>
   </div>;
 };
 export default Home;

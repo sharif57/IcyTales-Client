a08bec7f5b6e3fdb0190e8cd0f9a362973037456
sharif57 <sharifmahamud577951@gmail.com>
2024-11-07 18:14:22 +0600

    review section done

--- src/Page/Review.jsx
+++ src/Page/Review.jsx
@@ -1,6 +1,30 @@
 import { Link } from "react-router-dom";
 
+
 const Review = () => {
+
+    const items = [
+        {
+            num: '91+',
+            name: 'Kevin Andrew',
+            title: 'Quisuam est rui dolorem ipsum rui do sitamet, consectetur, adipise velit seu non numquam eiusm temora incidunt autlabore siner...'
+        },
+        {
+            num: '49%',
+            title: 'Ruisuam est rui dolorem ipsum rui do sit amet, consectetur, adipise velit seu non numquam eiusm temora incidunt autlabore siner...',
+            name: 'Peri James',
+            passion: 'Happy Client'
+        },
+        {
+            num: '20+',
+            title: 'Nuisuam est rui dolorem ipsum rui do sitamet, consectetur, adipise velit seu non numquam eiusm temora incidunt autlabore siner...',
+            name: 'Naurth Reough',
+            passion: 'Happy Client'
+        },
+
+    ]
+
+
     return <div>
         <div className="bg-gradient-to-b from-pink-100 to-pink-50 py-56">
             <div>
@@ -23,7 +47,32 @@ const Review = () => {
             </div>
         </div>
 
+        <div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 w-5/6 mx-auto mt-16 gap-8">
+            {
+                items.map((item, index) => <div key={index} className="p-6 border-b-4 space-y-3 border-pink-600 shadow-lg rounded-lg text-center rounded-l-2xl">
+                    <img src="/Figure → testimonial3-quoteimage.png.png" alt="" />
+                    <p className="text-start">{item.title}</p>
 
+                    <div className="flex items-center gap-5 ">
+                        <img src="/Figure → testimonial3-personimage1.jpg.png" alt="" />
+                        <div className="space-y-2">
+                            <div className="flex gap-1">
+                                <img src="/Symbol.png" alt="" />
+                                <img src="/Symbol.png" alt="" />
+                                <img src="/Symbol.png" alt="" />
+                                <img src="/Symbol.png" alt="" />
+                                <img src="/Symbol.png" alt="" />
+                            </div>
+                            <h1 className="text-pink-500 text-2xl font-semibold">{item.name}</h1>
+                            <p className="text-gray-500">Happy Client</p>
+                        </div>
+                    </div>
+                </div>)
+            }
+       
+        </div>
+        </div>
     </div>;
 };
 export default Review;
